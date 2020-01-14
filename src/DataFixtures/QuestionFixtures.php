08c06d8c3f5076967a62c1270d9f93322dcb562b
WojtekPilich <chilip@tlen.pl>
2020-01-14 22:04:45 +0100

    refactor and improve fixtures: use DependentFixtureInterface + references

--- src/DataFixtures/QuestionFixtures.php
+++ src/DataFixtures/QuestionFixtures.php
@@ -3,82 +3,87 @@
 namespace App\DataFixtures;
 
 use App\Entity\Question;
-use App\Entity\Questioner;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class QuestionFixtures extends Fixture
+class QuestionFixtures extends Fixture implements DependentFixtureInterface
 {
-    private static $questionBlock1 = [
+    /**
+     * First set of questions.
+     * @var array
+     */
+    private static $bobQuestions = [
         'Which stormtrooper wasn\'t able to complete his mission in "Star Wars: The Force Awakens?"',
         'What kind of vehicle did Rey live in?',
         'According to Master Yoda, how many Sith are always out there?',
     ];
 
-    private static $questionBlock2 = [
+    /**
+     * Second set of questions.
+     * @var array
+     */
+    private static $jackQuestions = [
         'What happened to Anakin Skywalker during the battle with Count Dooku?',
         'Who played the part of Commander Cody?',
         'Who killed Jabba?',
     ];
 
-    private static $questionBlock3 = [
+    /**
+     * Third set of questions.
+     * @var array
+     */
+    private static $jennyQuestions = [
         'What did Luke Skywalker lose in his fight with Darth Vader?',
         'According to the Emperor, what was Luke Skywalker\'s weakness?',
         'Where did the Clone Wars begin?',
         'What did Owen Lars tell Luke Skywalker about his father?',
     ];
 
+    /**
+     * Creates question objects binds them to questioners using references
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
-        $questioner1 = new Questioner();
-        $questioner1
-            ->setEmail('bob@example.com')
-            ->setName('Bob')
-            ->setNick('BigRobo');
-
-        $questioner2 = new Questioner();
-        $questioner2
-            ->setEmail('jack@example.com')
-            ->setName('Jack')
-            ->setNick('SmallHit');
-
-        $questioner3 = new Questioner();
-        $questioner3
-            ->setEmail('jenny@example.com')
-            ->setName('Jenny')
-            ->setNick('NiceTit');
-
-        $manager->persist($questioner1);
-        $manager->persist($questioner2);
-        $manager->persist($questioner3);
-
-        foreach (self::$questionBlock1 as $content) {
+        foreach (self::$bobQuestions as $content) {
             $question = new Question();
             $question
                 ->setContent($content);
 
             $manager->persist($question);
-            $questioner1->addQuestion($question);
+            $this->getReference(QuestionerFixtures::BOB_REFERENCE)->addQuestion($question);
         }
 
-        foreach (self::$questionBlock2 as $content) {
+        foreach (self::$jackQuestions as $content) {
             $question = new Question();
             $question
                 ->setContent($content);
 
             $manager->persist($question);
-            $questioner2->addQuestion($question);
+            $this->getReference(QuestionerFixtures::JACK_REFERENCE)->addQuestion($question);
         }
 
-        foreach (self::$questionBlock3 as $content) {
-            $question = new Question();
+        foreach (self::$jennyQuestions as $content) {
+            $question = (new Question());
             $question
                 ->setContent($content);
 
             $manager->persist($question);
-            $questioner3->addQuestion($question);
+            $this->getReference(QuestionerFixtures::JENNY_REFERENCE)->addQuestion($question);
         }
 
         $manager->flush();
+    }
+
+    /**
+     * Runs Questioner Fixtures first.
+     * @inheritDoc
+     */
+    public function getDependencies()
+    {
+        return [
+            QuestionerFixtures::class,
+        ];
     }
 }
