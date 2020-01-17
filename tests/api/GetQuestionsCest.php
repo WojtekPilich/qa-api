068a7d6194af9bb3f01ae8669a183b15952fc311
WojtekPilich <chilip@tlen.pl>
2020-01-17 20:09:06 +0100

    first GET test - questions without scope

--- tests/api/GetQuestionsCest.php
+++ tests/api/GetQuestionsCest.php
@@ -1,23 +1,33 @@
 <?php namespace App\Tests;
-use App\Tests\ApiTester;
+use App\DataFixtures\QuestionFixtures;
+use App\Entity\Questioner;
 use Codeception\Util\HttpCode;
 
 class GetQuestionsCest
 {
     public function _before(ApiTester $I)
     {
-
+        $I->loadFixtures(QuestionFixtures::class);
     }
 
-    // tests
-    public function getAllQuestionsTest(ApiTester $I)
+    /**
+     * Test getting all questions without scope
+     * @param ApiTester $I
+     */
+    public function getQuestionsWithoutScope(ApiTester $I)
     {
         $I->haveHttpHeader('accept', 'application/json');
         $I->haveHttpHeader('content-type', 'application/json');
         $I->sendGET('/questions');
-        $I->seeResponseCodeIs(HttpCode::OK);
         $I->seeResponseIsJson();
-        $I->seeResponseContainsJson(['id' => 74]);
-        $I->seeInDatabase('question', ['id' => 74]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseContainsJson(['question' => []]);
+
+        $I->dontSeeResponseCodeIs(HttpCode::BAD_REQUEST);
+        $I->dontSeeResponseContainsJson(['answers' => []]);
+        $I->dontSeeResponseContainsJson(['questioner' => []]);
+
+        $I->seeInRepository(Questioner::class, ['name' => 'Bob']);
     }
 }
