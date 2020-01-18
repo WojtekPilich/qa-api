8f68f78af9b882bc4a7dd3403b952dd396bc1496
WojtekPilich <chilip@tlen.pl>
2020-01-19 00:34:56 +0100

    change GetQuestionsAction after creating handler

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,9 +5,8 @@ namespace App\Controller;
 use App\Entity\Answer;
 use App\Entity\Answerer;
 use App\Entity\Question;
+use App\Message\GetQuestions;
 use App\Repository\QuestionRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\NonUniqueResultException;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
@@ -16,6 +15,8 @@ use FOS\RestBundle\Request\ParamFetcher;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -24,16 +25,16 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class QuestionController extends AbstractFOSRestController
 {
-    /** @var QuestionRepository $repository */
-    private $repository;
+    /** @var MessageBusInterface $messageBus */
+    private $messageBus;
 
     /**
      * QuestionController constructor.
-     * @param QuestionRepository $repository
+     * @param MessageBusInterface $messageBus
      */
-    public function __construct(QuestionRepository $repository)
+    public function __construct(MessageBusInterface $messageBus, QuestionRepository $repository)
     {
-        $this->repository = $repository;
+        $this->messageBus = $messageBus;
     }
 
     /**
@@ -45,23 +46,10 @@ class QuestionController extends AbstractFOSRestController
      */
     public function getQuestionsAction(ParamFetcher $paramFetcher): Response
     {
-        $questions = $this->repository->getAllQuestionsWithScope($paramFetcher->get('scope') ?? null);
+        $envelope = $this->messageBus->dispatch(new GetQuestions($paramFetcher->get('scope')));
+        $handledStamp = $envelope->last(HandledStamp::class);
 
-        if ($paramFetcher->get('scope') === "") {
-            return new JsonResponse([
-                'Status' => 'Bad request',
-                'Details' => 'Wrong query parameters.'
-                ], Response::HTTP_BAD_REQUEST);
-        }
-
-        if (empty($questions)) {
-            return new JsonResponse([
-                'Status' => 'Not found',
-                'Details' => 'No questions to show.'],
-                Response::HTTP_NOT_FOUND);
-        }
-
-        return $this->handleView($this->view($questions, Response::HTTP_OK));
+        return $handledStamp->getResult();
     }
 
     /**
@@ -70,7 +58,6 @@ class QuestionController extends AbstractFOSRestController
      * @param Request $request
      * @param int $id
      * @return Response
-     * @throws NonUniqueResultException
      */
     public function getQuestionAction(Request $request, int $id): Response
     {
