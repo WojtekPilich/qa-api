8c929c90b0245d20b2a9a412336de1bd2fb629be
WojtekPilich <chilip@tlen.pl>
2020-01-19 21:09:17 +0100

    a bit clean up

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Answer;
-use App\Entity\Answerer;
-use App\Entity\Question;
 use App\Message\Command\AddAnswer;
 use App\Message\Query\GetQuestion;
 use App\Message\Query\GetQuestions;
@@ -13,10 +10,8 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\Controller\Annotations\RequestParam;
 use FOS\RestBundle\Request\ParamFetcher;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Messenger\HandleTrait;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
