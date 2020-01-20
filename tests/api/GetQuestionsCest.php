02fa7ed23dd68f9fa5f0fd02b1330d5a350ac1ed
WojtekPilich <chilip@tlen.pl>
2020-01-20 20:01:12 +0100

    finished GET questions testing

--- tests/api/GetQuestionsCest.php
+++ tests/api/GetQuestionsCest.php
@@ -1,4 +1,5 @@
 <?php namespace App\Tests;
+use App\DataFixtures\QuestionerFixtures;
 use App\DataFixtures\QuestionFixtures;
 use Codeception\Util\HttpCode;
 
@@ -6,6 +7,7 @@ class GetQuestionsCest
 {
     public function _before(ApiTester $I)
     {
+        $I->loadFixtures(QuestionerFixtures::class);
         $I->loadFixtures(QuestionFixtures::class);
     }
 
@@ -39,8 +41,60 @@ class GetQuestionsCest
         $I->seeResponseCodeIs(HttpCode::OK);
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseContainsJson(['questioner' => []]);
+        $I->seeResponseContainsJson(['name' => 'Jenny']);
 
         $I->dontSeeResponseCodeIs(HttpCode::BAD_REQUEST);
         $I->dontSeeResponseContainsJson(['answers' => []]);
+    }
+
+    /**
+     * @param ApiTester $I
+     */
+    public function getQuestionsWithAnswersScope(ApiTester $I)
+    {
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('content-type', 'application/json');
+        $I->sendGET('/questions', ['scope[]' => 'answers']);
+        $I->seeResponseIsJson();
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseContainsJson(['answers' => []]);
+
+        $I->dontSeeResponseCodeIs(HttpCode::BAD_REQUEST);
+        $I->dontSeeResponseContainsJson(['questioner' => []]);
+    }
+
+    /**
+     * @param ApiTester $I
+     */
+    public function getQuestionsWithFullScope(ApiTester $I)
+    {
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('content-type', 'application/json');
+        $I->sendGET('/questions?scope[]=author&scope[]=answers');
+        $I->seeResponseIsJson();
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseContainsJson(['answers' => []]);
+        $I->seeResponseContainsJson(['questioner' => []]);
+
+        $I->dontSeeResponseCodeIs(HttpCode::BAD_REQUEST);
+    }
+
+    /**
+     * @param ApiTester $I
+     */
+    public function getQuestionsWithWrongScopeParameter(ApiTester $I)
+    {
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('content-type', 'application/json');
+        $I->sendGET('/questions', ['scope[]' => 'xxx']);
+        $I->seeResponseIsJson();
+        $I->seeResponseCodeIs(HttpCode::BAD_REQUEST);
+        $I->seeResponseCodeIsClientError();
+        $I->seeResponseContainsJson(['details' => 'wrong query parameters.']);
+
+        $I->dontSeeResponseCodeIs(HttpCode::OK);
+        $I->dontSeeResponseContainsJson(['questioner' => []]);
     }
 }
