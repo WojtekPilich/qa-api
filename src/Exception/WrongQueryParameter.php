f86e1e9644a08677d8aa75046b39f1fe9ddba17e
WojtekPilich <chilip@tlen.pl>
2020-02-12 22:09:40 +0100

    improve exception

--- src/Exception/WrongQueryParameter.php
+++ src/Exception/WrongQueryParameter.php
@@ -1,11 +1,13 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
+
 final class WrongQueryParameter extends Exception
 {
     public static function with(string $key)
     {
         $message = sprintf('Wrong query parameter: %s', $key);
 
-        return new self($message);
+        return new self($message, Response::HTTP_BAD_REQUEST);
     }
 }
