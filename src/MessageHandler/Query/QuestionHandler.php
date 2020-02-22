bbdbe6936bec3a016969dd49ce809d6a59d680c4
WojtekPilich <chilip@tlen.pl>
2020-02-22 17:20:30 +0100

    make handler final class

--- src/MessageHandler/Query/QuestionHandler.php
+++ src/MessageHandler/Query/QuestionHandler.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-class QuestionHandler implements MessageHandlerInterface
+final class QuestionHandler implements MessageHandlerInterface
 {
     /** @var QuestionRepository $repository */
     private $repository;
