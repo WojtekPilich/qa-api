a2a7004d1d9ef1b74f2fb9b916c3d1b308658062
WojtekPilich <chilip@tlen.pl>
2020-02-27 16:13:10 +0100

    change interface name for validator

--- src/Validator/QuestionsValidator.php
+++ src/Validator/QuestionsValidator.php
@@ -5,14 +5,14 @@ namespace App\Validator;
 use App\Scope\Scope;
 use WrongQueryParameter;
 
-final class QuestionsValidator implements ValidatorInterface
+final class QuestionsValidator implements Verifiable
 {
     /**
      * @param Scope $scope
      * @return ValidScope
      * @throws WrongQueryParameter
      */
-    public function validate(Scope $scope): ValidScope
+    public function validate($scope): ValidScope
     {
         foreach ($scope->parameters() as $parameter) {
             if ($parameter !== 'author' && $parameter !== 'answers') {
